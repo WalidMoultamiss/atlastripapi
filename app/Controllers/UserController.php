9076ef26a7e3c8696e4530dc8c5a6ca9eddb6a31
Yusfuu <youssefhajjari01@gmail.com>
2021-07-10 01:08:51 +0100

    work

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -26,6 +26,7 @@ class UserController extends Controller
     ];
 
     $middle = (object) $middleware->validate($user, $rules);
+    print_r([$user->password, $userFind[0]['password']]);
 
     if ($middle->error) {
       Response::json($middle);
@@ -33,11 +34,14 @@ class UserController extends Controller
       if (!count($userFind)) {
         return Response::json(["message" => "Email Not Found"]);
       } else {
-        echo "aeaeea";
+        echo "1";
+
         if (password_hash($user->password, $userFind[0]['password'])) {
+          echo "2";
           unset($userFind[0]['password']);
           $response = \App\Http\Middleware\Auth::create($userFind[0]);
           $response->dd = $userFind[0];
+          print_r($response);
           Response::json($response);
         } else {
           Response::json(["message" => "password incorrect"]);
